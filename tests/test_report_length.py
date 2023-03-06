025409dc049d8c5aaa7ae2acd36768dcc93d9c18
= <=>
2023-03-06 17:13:43 +0000

    and again

--- tests/test_report_length.py
+++ tests/test_report_length.py
@@ -1,13 +1,14 @@
 from lib.report_length import *
 
-def six_letter_word():
+def test_six_letter_word():
     result = report_length("snazzy")
     assert result == 6
 
-def five_letter_word():
+def test_five_letter_word():
     result = report_length("house")
     assert result == 5
 
-def ten_letter_word():
+def test_ten_letter_word():
     result = report_length("pizzicatos")
     assert result == 10
+    
