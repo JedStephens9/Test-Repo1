71db438cfa3e90f2c14ffe81e97c4678026ca59a
= <=>
2023-03-07 14:09:23 +0000

    chips

--- tests/test_gratitudes.py
+++ tests/test_gratitudes.py
@@ -2,10 +2,16 @@ from lib.gratitudes import *
 
 def test_gratitudes_returns_no_additional_string_if_one_not_given():
     gratitudes = Gratitudes()
-    gratitudes.add = ("")
+    gratitudes.add ("")
     assert gratitudes.format() == "Be grateful for: "
 
 def test_gratitudes_returns_one_additional_string_if_one_given():
     gratitudes = Gratitudes()
-    gratitudes.add = ("beer")
+    gratitudes.add ("beer")
     assert gratitudes.format() == "Be grateful for: beer"
+
+def test_gratitudes_returns_two_additional_string_if_two_given():
+    gratitudes = Gratitudes()
+    gratitudes.add ("beer")
+    gratitudes.add ("chips")
+    assert gratitudes.format() == "Be grateful for: beer, chips"
